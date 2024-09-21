4f47de75d095dcc1877e9df5d16796f7f0e0e6c0
tonybnya <nya.tony2010@gmail.com>
2024-09-21 10:11:46 +0100

    style: add filter to the social icons on hover

--- src/components/SocialHero.jsx
+++ src/components/SocialHero.jsx
@@ -6,9 +6,9 @@ import twitter from "../assets/icons/twitter.png";
 const SocialHero = () => {
   return (
     <div className="container pl-6 flex xl:flex-col xl:gap-20 md:flex-row md:justify-center md:gap-10 max-sm:flex-row max-sm:justify-center max-sm:gap-10">
-        <NavLink to="https://web.facebook.com/tonybnya" target="_blank"><img src={facebook} alt="Facebook logo" /></NavLink>
-        <NavLink to="https://www.instagram.com/tonybnya" target="_blank"><img src={instagram} alt="Instagram logo" /></NavLink>
-        <NavLink to="https://x.com/tonybnya" target="_blank"><img src={twitter} alt="Twitter logo" /></NavLink>
+        <NavLink to="https://web.facebook.com/tonybnya" target="_blank"><img src={facebook} className="logo" alt="Facebook logo" /></NavLink>
+        <NavLink to="https://www.instagram.com/tonybnya" target="_blank"><img src={instagram} className="logo" alt="Instagram logo" /></NavLink>
+        <NavLink to="https://x.com/tonybnya" target="_blank"><img src={twitter} className="logo" alt="Twitter logo" /></NavLink>
     </div>
   )
 }
