2e95b8e2b8ba7844472213765877ce34a6fcafc8
tonybnya <nya.tony2010@gmail.com>
2024-09-26 17:16:16 +0100

    fix: change logo width and adjust the flex properties of the nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,10 @@ import close from "../assets/icons/close.png";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  // eslint-disable-next-line react/prop-types
   const RSLink = ({ title }) => (
     <LinkScroll
-      className="base-bold text-p4 uppercase transition-colors duration-1000 cursor-pointer hover:text-g8 max-lg:my-4 max-lg:h5"
+      className="base-bold text-p4 uppercase transition-colors duration-1000 cursor-pointer hover:text-[#862019] max-lg:my-4 max-lg:h5"
       onClick={() => setIsOpen(false)}
       to={title}
     >
@@ -20,10 +21,11 @@ const Navbar = () => {
   );
 
   return (
-    <div className="fixed top-0 left-0 z-50 w-full py-10">
-      <div className="container flex h-14 justify-center items-center max-lg:px-5">
+    <div className="fixed top-0 left-0 right-0 z-50 w-full py-10">
+      <div className="container flex h-10 justify-center items-center max-lg:px-5 lg:px-5">
         <NavLink to="/" className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src={logo} alt="Superman white logo" width={90} height={55} />
+          <img src={logo} alt="Superman logo" width={60} height={25} />
+          {/* <img src={logo} alt="Superman white logo" className="max-lg:w-30 lg:w-40" /> */}
         </NavLink>
         <div
           className={clsx(
@@ -41,18 +43,18 @@ const Navbar = () => {
                     spy
                     smooth
                     className={clsx(
-                      "max-lg:hidden transition-transform duration-500 cursor-pointer",
+                      "max-lg:hidden transition-transform duration-1000 cursor-pointer",
                     )}
                   >
                     <img
                       src={logo}
-                      alt="Superman white logo"
-                      width={160}
+                      alt="Superman logo"
+                      width={80}
                       height={55}
                     />
                   </LinkScroll>
                 </li>
-                <li className="nav-li">
+                <li className="nav-li flex gap-10">
                   <RSLink title="home" />
                   <div className="dot" />
                   <RSLink title="eshop" />
