9507983727b871bcbad28b72c2478131870e51ca
tonybnya <nya.tony2010@gmail.com>
2024-09-27 15:52:24 +0100

    feat: complete the batman vs superman section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroHome from "../sections/HeroHome.jsx";
 import ActorsHome from "../sections/ActorsHome.jsx";
+import MultimediaHome from "../sections/MultimediaHome.jsx";
 
 const Home = () => {
   return (
     <main className="overflow-hidden">
       <HeroHome />
       <ActorsHome />
+      <MultimediaHome />
     </main>
   );
 };
---
+++ src/sections/MultimediaHome.jsx
@@ -0,0 +1,27 @@
+import bg from "../assets/images/img11.png";
+import { multimedia } from "../constants";
+
+const MultimediaHome = () => {
+  const multimediaItem = multimedia[0];
+
+  return (
+    <section
+      style={{ backgroundImage: `url(${bg})` }}
+      className="bg-cover bg-center bg-no-repeat drop-shadow-red"
+    >
+      <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-90"></div>
+      <div className="relative container bg-black/90 py-10">
+        <h1 className="uppercase font-bold text-4xl">{multimediaItem.title}</h1>
+        {multimediaItem.text.map((paragraph, index) => (
+          <p key={index} className="py-2 text-justify font-rajdhani text-xl">
+            {paragraph}
+          </p>
+        ))}
+        <div className="py-10">
+          <h1 className="uppercase font-bold text-4xl">multimedia</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default MultimediaHome;
