b8599a2896e2b676fbf5ee6500b0c62e1f75a235
tonybnya <nya.tony2010@gmail.com>
2024-09-29 15:39:32 +0100

    style: change the code of the slider and quotes svg images

--- src/components/QuoteSlider.jsx
+++ src/components/QuoteSlider.jsx
@@ -10,18 +10,22 @@ const QuoteSlider = () => {
       <img
         src={quoteLeft}
         alt="left quote"
-        className="absolute top-0 left-0 h-8 w-8 sm:h-12 sm:w-12 lg:h-16 lg:w-16 cursor-pointer z-10"
+        className="text-red absolute top-0 left-0 h-8 w-8 sm:h-12 sm:w-12 lg:h-16 lg:w-16 cursor-pointer z-10"
       />
 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 sm:w-16 sm:h-16 lg:w-20 lg:h-20 flex items-center justify-center bg-green-500 text-white font-bold">
-        <p>{quote.text}</p>
-        <span>{quote.source}</span>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-40 sm:w-56 lg:w-72 px-4 py-4 flex flex-col items-center justify-center bg-[#07338C] text-white text-center font-bold rounded-lg shadow-lg">
+        <blockquote className="font-cantarell text-xs sm:text-sm lg:text-lg mb-2 italic">
+          {quote.text}
+        </blockquote>
+        <cite className="font-advent font-bold text-xs sm:text-sm lg:text-base not-italic">
+          - {quote.source}
+        </cite>
       </div>
 
       <img
         src={quoteRight}
         alt="right quote"
-        className="absolute bottom-0 right-0 h-8 w-8 sm:h-12 sm:w-12 lg:h-16 lg:w-16 cursor-pointer z-10"
+        className="text-red absolute bottom-0 right-0 h-8 w-8 sm:h-12 sm:w-12 lg:h-16 lg:w-16 cursor-pointer z-10"
       />
     </div>
   );
