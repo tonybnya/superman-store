035856a41933187e2485fe86e3a36bdfed83549d
tonybnya <nya.tony2010@gmail.com>
2024-09-29 14:29:05 +0100

    refactor: create a MultimediaSlider component for the slider of the Multimedia section on the home page

---
+++ src/components/MultimediaSlider.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import leftArrow from "../assets/icons/left.png";
+import rightArrow from "../assets/icons/right.png";
+import MultimediaCard from "./MultimediaCard";
+
+const MultimediaSlider = ({movies}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === movies.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? movies.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const autoSlide = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearInterval(autoSlide);
+  }, []);
+
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <img
+        src={leftArrow}
+        alt="left arrow"
+        onClick={prevSlide}
+        className="absolute left-0 z-10 h-8 w-8 cursor-pointer md:h-12 md:w-12"
+      />
+      <div className="w-full max-w-[36rem] overflow-hidden">
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {movies.map((movie) => (
+            <div key={movie.id} className="min-w-full">
+              <MultimediaCard movie={movie} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <img
+        src={rightArrow}
+        alt="right arrow"
+        onClick={nextSlide}
+        className="absolute right-0 z-10 h-8 w-8 cursor-pointer md:h-12 md:w-12"
+      />
+    </div>
+  )
+}
+
+export default MultimediaSlider;
--- src/sections/MultimediaHome.jsx
+++ src/sections/MultimediaHome.jsx
@@ -1,32 +1,9 @@
 import bg from "../assets/images/img11.png";
-import leftArrow from "../assets/icons/left.png";
-import rightArrow from "../assets/icons/right.png";
-import { useState, useEffect } from "react";
 import { multimedia, movies } from "../constants";
-import MultimediaCard from "../components/MultimediaCard.jsx";
+import MultimediaSlider from "../components/MultimediaSlider.jsx";
 
 const MultimediaHome = () => {
   const multimediaItem = multimedia[0];
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === movies.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? movies.length - 1 : prevIndex - 1
-    );
-  };
-
-  useEffect(() => {
-    const autoSlide = setInterval(() => {
-      nextSlide();
-    }, 5000);
-    return () => clearInterval(autoSlide);
-  }, []);
 
   return (
     <section
@@ -44,33 +21,7 @@ const MultimediaHome = () => {
         <div className="py-10">
           <h1 className="uppercase font-bold text-4xl">multimedia</h1>
         </div>
-        {/* TODO: Slider here */}
-        <div className="relative w-full flex items-center justify-center">
-          <img
-            src={leftArrow}
-            alt="left arrow"
-            onClick={prevSlide}
-            className="absolute left-0 z-10 h-8 w-8 cursor-pointer md:h-12 md:w-12"
-          />
-          <div className="w-full max-w-[36rem] overflow-hidden">
-            <div
-              className="flex transition-transform duration-500 ease-in-out"
-              style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-            >
-              {movies.map((movie) => (
-                <div key={movie.id} className="min-w-full">
-                  <MultimediaCard movie={movie} />
-                </div>
-              ))}
-            </div>
-          </div>
-          <img
-            src={rightArrow}
-            alt="right arrow"
-            onClick={nextSlide}
-            className="absolute right-0 z-10 h-8 w-8 cursor-pointer md:h-12 md:w-12"
-          />
-        </div>
+        <MultimediaSlider movies={movies} />
       </div>
     </section>
   );
