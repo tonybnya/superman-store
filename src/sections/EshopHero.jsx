a2911b6f83d879b1e2c6eaa84521c6749c7db09b
tonybnya <nya.tony2010@gmail.com>
2024-10-10 20:36:03 +0100

    style: update the styles of the hero section of the Eshop page

--- src/sections/EshopHero.jsx
+++ src/sections/EshopHero.jsx
@@ -4,12 +4,18 @@ import logo from "../assets/logos/logo.png";
 const HeroEshop = () => {
   return (
     <header
-      style={{ backgroundImage: `url(${bg})` }}
-      className="bg-cover bg-center bg-no-repeat drop-shadow-custom min-h-screen flex flex-col gap-4 justify-center"
+      className="relative bg-cover bg-center bg-no-repeat drop-shadow-blue min-h-screen flex flex-col gap-4 justify-center"
     >
-      <h1 className="text-center text-[#F1C530] font-bold font-advent lg:text-8xl max-lg:text-6xl md:text-8xl">Looking for<br />a yellow underwear?</h1>
-      <div className="flex items-center justify-center">
-        <img src={logo} className="animate-bounce w-20 h-20" alt="Colorize superman logo" />
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-30"
+        style={{backgroundImage: `url(${bg})`}}
+      ></div>
+      <div className="relative z-10">
+        <h1 className="text-center text-[#F1C530] font-bold font-advent lg:text-8xl max-lg:text-6xl md:text-8xl">Looking for<br />a yellow underwear?</h1>
+        <h3 className="font-rajdhani text-center text-white py-4 lg:text-6xl max-lg:text-xl md:text-4xl">Get the superman flow with this e-shop!</h3>
+        <div className="flex items-center justify-center">
+          <img src={logo} className="animate-bounce w-20 h-20" alt="Colorize superman logo" />
+        </div>
       </div>
     </header>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,7 @@ export default {
       dropShadow: {
         custom: "50px 50px 100px white",
         red: "50px 50px 100px #862019",
+        blue: "50px 50px 50px #062A5A",
         other: "10px 10px 50px #862019",
       },
       fontFamily: {
