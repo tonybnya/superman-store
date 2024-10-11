24933b899841468796d7dff22bd8d4f07b27360f
tonybnya <nya.tony2010@gmail.com>
2024-10-11 21:13:30 +0100

    feat: fetch the products from the FastAPI backend to the Catalog of the Eshop page

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://superman-dqt1hvkg6-tonybnyas-projects.vercel.app",
+  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "https://superman-dqt1hvkg6-tonybnyas-projects.vercel.app",
 })
 
 export default api;
--- src/sections/EshopCatalog.jsx
+++ src/sections/EshopCatalog.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
 const EshopCatalog = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await api.get("/products");
+    setProducts(response.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div>EshopCatalog</div>
+    <div className="container">
+      <h1>EshopCatalog</h1>
+      {console.log(products)}
+    </div>
   )
 }
 
